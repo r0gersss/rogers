50f9706b2ae4cea3f50f720034dac8c02cc45901
r0gersss <145066690+r0gersss@users.noreply.github.com>
2023-09-14 16:44:34 -0400

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_cczcb0i", "template_k7ixms9", formData)
+    emailjs.send("service_lduv9q9", "template_89utyxc", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
